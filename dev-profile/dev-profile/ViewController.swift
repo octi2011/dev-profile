7ab067f0ad30b8aa3bfee5ae5f7516ccfb62082e
Octavian <octavian@192-168-0-105.rdsnet.ro>
2017-12-28 18:19:39 +0200

    removed stack width constraint

--- dev-profile/dev-profile/ViewController.swift
+++ dev-profile/dev-profile/ViewController.swift
@@ -10,11 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var profileImgView: UIImageView!
+    @IBOutlet weak var dataImg: UIImageView!
+    @IBOutlet weak var imageImg: UIImageView!
+    @IBOutlet weak var videosImg: UIImageView!
+    @IBOutlet weak var codeImg: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         Helpers.makeImageCornersRound(radius: 8, image: profileImgView)
+        Helpers.makeImageCornersRound(radius: 8, image: dataImg)
+        Helpers.makeImageCornersRound(radius: 8, image: imageImg)
+        Helpers.makeImageCornersRound(radius: 8, image: videosImg)
+        Helpers.makeImageCornersRound(radius: 8, image: codeImg)
     }
     
     //in stackViews you can only add constraints to the first element whne fill equally is checked
